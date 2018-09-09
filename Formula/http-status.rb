28b91d858446539dfab80d31061b1815f25447b7
Florian Kaiser <florian.kaiser@fnkr.net>
2018-09-09 10:08:35 +0200

    Update formula http-status

--- Formula/http-status.rb
+++ Formula/http-status.rb
@@ -4,8 +4,8 @@ class HttpStatus < Formula
   url "https://dotfiles.fnkr.net/bin/http-status",
       :using => :nounzip
 
-  version "1"
-  sha256 "1d2514db4f3a36e6c36ad5d2a65d10a6964652c97473884779f4c48e4fc679f4"
+  version "2"
+  sha256 "77265e248230140ae7b4dab3fdef44a213d81160a339a5046d06e26e34b4946d"
 
   bottle :unneeded
 
