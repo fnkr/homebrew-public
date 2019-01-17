c9d02cc5a95e15794bb8125dcdf627b5a19bab91
Florian Kaiser <florian.kaiser@fnkr.net>
2019-01-17 10:38:45 +0000

    Update proserver.rb

--- Formula/proserver.rb
+++ Formula/proserver.rb
@@ -4,8 +4,8 @@ class Proserver < Formula
   url "https://dotfiles.fnkr.net/bin/proserver",
       :using => :nounzip
 
-  version "8"
-  sha256 "c618135af2f010cae5f8d2d5349e31d87bc789a74d9142ef240aeceb13cc01a7"
+  version "9"
+  sha256 "24625e2651ffb691f126e371020aaecbaddf2084baaa3f11a2fb9b130c58c37d"
 
   bottle :unneeded
 
