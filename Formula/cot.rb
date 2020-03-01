7884d4d4945c0ab9c100f4dc1b3e32101a7baf2e
Florian Kaiser <florian.kaiser@fnkr.net>
2020-03-01 11:54:10 +0000

    Update Formula/cot.rb

--- Formula/cot.rb
+++ Formula/cot.rb
@@ -1,9 +1,9 @@
 class Cot < Formula
   desc "A convenient way to containerize command line applications with Docker."
   homepage "https://github.com/fnkr/cot"
-  url ""
-  sha256 ""
-  version ""
+  url "https://github.com/fnkr/cot/releases/download/v0.8/cot_darwin_amd64.tar.gz"
+  sha256 "db9cdd75b0bfc7198fa0c5411d55b7382d1d0e73140ef968519bcde5b555dc47"
+  version "v0.8"
 
   bottle :unneeded
 
